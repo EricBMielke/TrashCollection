f62ca5ded150d8050c9052d7df38871d767f11b5
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-08-06 11:20:40 -0500

    Adding the skeleton for the ability to have roles in the application

--- TrashCollection/Startup.cs
+++ TrashCollection/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TrashCollection.Models;
 
 [assembly: OwinStartupAttribute(typeof(TrashCollection.Startup))]
 namespace TrashCollection
@@ -9,6 +12,60 @@ namespace TrashCollection
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
+        }
+        private void createRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "EricMielke";
+                user.Email = "EricBMielke@gmail.com";
+
+                string userPWD = "EricMielke12345";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("TrashManager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "TrashManager";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("TrashCustomer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "TrashCustomer";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
